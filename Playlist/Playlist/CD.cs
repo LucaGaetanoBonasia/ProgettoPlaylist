ba7a2461ba2f3a8d3c5010863199f97f15f57dbb
Matteo Bondi <MATTEO.BONDI@local.ittcesena.it>
2025-10-31 09:38:06 +0100

    R0.1 prima versione classe cd

--- Playlist/Playlist/CD.cs
+++ Playlist/Playlist/CD.cs
@@ -8,5 +8,46 @@ namespace Playlist
 {
     class CD
     {
+        private string _titolo;
+        private string _autore;
+        private List<Brani> _brani;
+
+        public CD(string titolo, string autore)
+        {
+            _titolo = titolo;
+            _autore = autore;
+            _brani = new List<Brani>();
+        }
+        public string GetTitolo()
+        {
+            return _titolo;
+        }
+        public void SetTitolo(string titolo)
+        {
+            _titolo = titolo;
+        }
+        public string GetAutore()
+        {
+            return _autore;
+        }
+        public void SetAutore(string autore)
+        {
+            _autore = autore;
+        }
+        public void AggiungiBrano(Brani b)
+        {
+            _brani.Add(b);
+        }
+        public double Durata()
+        {
+            double totale = 0;
+            foreach (Brani b in _brani)
+            {
+                totale += b.GetDurata();
+            }
+            return totale;
+        }
+       
+
     }
 }
